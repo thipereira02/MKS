6305ffd0811fd19d1f3b73ebb7af9cc642419275
Thiago <thiago.spereira02@gmail.com>
2022-10-23 20:48:14 -0300

    editing header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,65 @@
+import styled from "styled-components";
+
+import { FaShoppingCart } from "react-icons/fa";
+
 export default function Header() {
     return (
-        <header>
-            <h1>Header</h1>
-        </header>
+        <Bar>
+            <Logo>
+                <h1>MKS</h1>
+                <h2>Sistemas</h2>
+            </Logo>
+            <Cart>
+                <FaShoppingCart />
+                <p>0</p>
+            </Cart>
+        </Bar>
     );
 }
+
+const Bar = styled.div`
+    background-color: #0F52BA;
+    height: 101px;
+    width: 100%;
+    position: fixed;
+    padding-left: 65px;
+    padding-right: 88px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+`;
+
+const Logo = styled.div`
+    display: flex;
+    align-items: end;
+    color: #FFF;
+
+    h1 {
+        font-size: 40px;
+        font-weight: 600;
+    }
+
+    h2{
+        font-size: 20px;
+        font-weight: 300;
+        margin-bottom: 6px;
+        margin-left: 8px;
+    }
+`;
+
+const Cart = styled.div`
+    width: 90px;
+    height: 45px;
+    background-color: #FFF;
+    border-radius: 8px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-left: 15px;
+    padding-right: 27px;
+
+    p{
+        font-size: 18px;
+        font-weight: 700;
+    }
+`;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,9 @@
+import Header from "../components/Header";
+
 export default function App() {
     return (
         <>
+          <Header />
           <h1>App</h1>
         </>
     );
